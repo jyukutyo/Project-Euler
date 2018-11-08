7f41b9b868ec761850094304c0ed273c518bdcc6
jyukutyo <sakatakoichi@gmail.com>
2018-11-08 14:04:02 +0900

    Fix a comment

--- src/main/scala/Problem3.scala
+++ src/main/scala/Problem3.scala
@@ -2,7 +2,7 @@
 object Problem3 extends App {
   lazy val ps: Stream[Int] = 2 #:: Stream.from(3).filter(
     i => {
-      // 素数だけのリストを作る。jはiの平方根より小さい数で、iが割り切れる数である。
+      // 素数だけのリストを作る。jはiの平方根より小さい数で、iを割り切ることができない数である。
       ps.takeWhile(j => { j * j <= i }).forall(i % _ > 0)
     }
   )
